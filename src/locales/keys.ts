28c621f205b881ce0c7cc78247ec1470553fb6ff
xzhiyouu <nelson970602@gmail.com>
2025-06-10 00:39:03 +0800

    Add files via upload

---
+++ src/locales/keys.ts
@@ -0,0 +1,29 @@
+enum I18nKeys {
+  copy_right_author = "copy_right_author",
+  copy_right_publish_date = "copy_right_publish_date",
+  copy_right_license = "copy_right_license",
+
+  nav_bar_home = "nav_bar_home",
+  nav_bar_archive = "nav_bar_archive",
+  nav_bar_about = "nav_bar_about",
+  nav_bar_github = "nav_bar_github",
+  nav_bar_search_placeholder = "nav_bar_search_placeholder",
+
+  post_card_words = "post_card_words",
+  post_card_minutes = "post_card_minutes",
+
+  side_bar_categories = "side_bar_categories",
+  side_bar_tags = "side_bar_tags",
+  side_bar_view_more = "side_bar_view_more",
+
+  archive_year_title_count = "archive_year_title_count",
+
+  pages_categories_archive = "pages_categories_archive",
+  pages_archive_archive = "pages_archive_archive",
+  pages_tags_archive = "pages_tags_archive",
+
+  pages_tags_title = "pages_tags_title",
+  pages_categories_title = "pages_categories_title",
+}
+
+export default I18nKeys;
---
+++ src/types/config.ts
@@ -0,0 +1,39 @@
+import type I18nKeys from "../locales/keys";
+
+interface Configuration {
+  title: string;
+  subTitle: string;
+  brandTitle: string;
+
+  description: string;
+
+  site: string;
+
+  locale: "en" | "zh-CN";
+
+  navigators: { nameKey: I18nKeys; href: string }[];
+
+  username: string;
+  sign: string;
+  avatarUrl: string;
+
+  socialLinks: { icon: string; link: string }[];
+
+  maxSidebarCategoryChip: number;
+  maxSidebarTagChip: number;
+  maxFooterCategoryChip: number;
+  maxFooterTagChip: number;
+
+  banners: string[];
+
+  slugMode: "HASH" | "RAW";
+
+  license: {
+    name: string;
+    url: string;
+  };
+
+  bannerStyle: "LOOP";
+}
+
+export type { Configuration };
