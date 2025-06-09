9f711df08e714c8ca4323c6028846a292505698c
xzhiyouu <nelson970602@gmail.com>
2025-06-10 01:32:32 +0800

    Update yukina.config.ts

--- yukina.config.ts
+++ yukina.config.ts
@@ -2,11 +2,11 @@ import I18nKeys from "./src/locales/keys";
 import type { Configuration } from "./src/types/config";
 
 const YukinaConfig: Configuration = {
-  title: "Yukina",
-  subTitle: "Yukina Template Demo Site",
-  brandTitle: "Yukina",
+  title: "Xzhiyouu's Blog",
+  subTitle: "This space is a record of quiet effort.",
+  brandTitle: "xzhiyouu",
 
-  description: "Demo Site",
+  description: "Personal Blog",
 
   site: "https://yukina-blog.vercel.app",
 
@@ -31,22 +31,22 @@ const YukinaConfig: Configuration = {
     },
   ],
 
-  username: "WhitePaper 白芷",
-  sign: "Ad Astra Per Aspera.",
-  avatarUrl: "https://s2.loli.net/2025/01/25/FPpTrQSezM8ivbl.webp",
+  username: "xzhiyouu",
+  sign: "A high school student learning cybersecurity.",
+  avatarUrl: "https://scontent.ftpe8-3.fna.fbcdn.net/v/t39.30808-6/495081349_1956045761870905_221920331418930353_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=ly-1NafnawUQ7kNvwG795YS&_nc_oc=AdlIo9ZdAL-ZWDaXNJqb6hY1oHiXKpNabdZJD-SWbVPqe8S0nVyK_wjeeJyhB0Ajywc&_nc_zt=23&_nc_ht=scontent.ftpe8-3.fna&_nc_gid=tE5iXuUfitlKZV9rnbXK9w&oh=00_AfOgbwws_r7onjMbAzyn2zaa8qBByauSAQWmJ6u3uItHIw&oe=684CDCDB",
   socialLinks: [
     {
       icon: "line-md:github-loop",
-      link: "https://github.com/WhitePaper233",
+      link: "https://github.com/xzhiyouu62",
     },
-    {
-      icon: "mingcute:bilibili-line",
-      link: "https://space.bilibili.com/22433608",
-    },
-    {
-      icon: "mingcute:netease-music-line",
-      link: "https://music.163.com/#/user/home?id=125291648",
-    },
+  {
+    icon: "mingcute:facebook-line", 
+    link: "https://www.facebook.com/nelson.hsieh.2025/",
+  },
+  {
+    icon: "mingcute:instagram-line", 
+    link: "https://www.instagram.com/xzhiyouu/",
+  },
   ],
   maxSidebarCategoryChip: 6, // It is recommended to set it to a common multiple of 2 and 3
   maxSidebarTagChip: 12,
@@ -54,14 +54,14 @@ const YukinaConfig: Configuration = {
   maxFooterTagChip: 24,
 
   banners: [
-    "https://s2.loli.net/2025/01/25/PBvHFjr5yDu6t4a.webp",
-    "https://s2.loli.net/2025/01/25/6bKcwHZigzlM4mJ.webp",
-    "https://s2.loli.net/2025/01/25/H9WgEK6qNTcpFiS.webp",
-    "https://s2.loli.net/2025/01/25/njNVtuUMzxs81RI.webp",
-    "https://s2.loli.net/2025/01/25/tozsJ8QHAjFN3Mm.webp",
-    "https://s2.loli.net/2025/01/25/Pm89OveZq7NWUxF.webp",
-    "https://s2.loli.net/2025/01/25/UCYKvc1ZhgPHB9m.webp",
-    "https://s2.loli.net/2025/01/25/JjpLOW8VSmufzlA.webp",
+    "https://i.pinimg.com/736x/1c/68/6e/1c686eea5580f43241c7fc23df12e698.jpg",
+    "https://i.pinimg.com/736x/e7/95/81/e7958142b5a4135a5494ceecd6016fb0.jpg",
+    "https://i.pinimg.com/736x/2e/40/dc/2e40dc63dacbb648982ddc2589a76356.jpg",
+    "https://i.pinimg.com/736x/a1/b2/dd/a1b2dd33e636981158b4b5b689896bed.jpg",
+    "https://i.pinimg.com/736x/ca/92/30/ca923012f2da62ba5ba20a99e6c0ff91.jpg",
+    "https://i.pinimg.com/736x/7f/f7/1e/7ff71e6d41a30a3e9058c91da4330760.jpg",
+    "https://i.pinimg.com/736x/aa/98/4d/aa984df5a8ce94689588b835a8754fd4.jpg",
+    "https://i.pinimg.com/736x/06/62/58/066258b891c677c7276a730c51a549c7.jpg",
   ],
 
   slugMode: "HASH", // 'RAW' | 'HASH'
